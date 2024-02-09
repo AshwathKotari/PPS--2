d4730d3d91462f35487d5af338250aa8eae1cd0a
AshwathKotari <ashwathkotari1996@gmail.com>
2024-02-09 11:37:58 +0530

    .active{}

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
 export const Navbar = () => {
   return (
     <>
         <nav className='links'>
-            <Link to='/'>Home</Link>
-            <Link to='/About'>About</Link>
-            <Link to='/Softskills'>Softskills</Link>
-            <Link to='/Techstack'>Techstack</Link>
-            <Link to='/ProjectList'>Projectlist</Link>
-            <Link to='/Contact'>Contact</Link>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/About'>About</NavLink>
+            <NavLink to='/Softskills'>Softskills</NavLink>
+            <NavLink to='/Techstack'>Techstack</NavLink>
+            <NavLink to='/ProjectList'>Projectlist</NavLink>
+            <NavLink to='/Contact'>Contact</NavLink>
             
         </nav>
     </>
