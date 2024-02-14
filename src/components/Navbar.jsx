921006eaea0cbd046e76b36275b28abb42721048
AshwathKotari <ashwathkotari1996@gmail.com>
2024-02-14 10:17:53 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,31 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export const Navbar = () => {
+    const [view,setview] = useState(false)
+
+    function ButtonClicked(){
+        setview(!view)
+    }
+    let responsivelinks = <nav className='links2'>
+                                <NavLink to='/'>Home</NavLink>
+                                <NavLink to='/About'>About</NavLink>
+                                <NavLink to='/Softskills'>Softskills</NavLink>
+                                <NavLink to='/Techstack'>Techstack</NavLink>
+                                <NavLink to='/ProjectList'>Projectlist</NavLink>
+                                {/* <NavLink to='/Contact'>Contact</NavLink> */}
+                                
+                            </nav>
+
   return (
     <>
+    {/* <div className='hamburgerdiv'>
+    <RxHamburgerMenu onClick={ButtonClicked} className='hamburger'/>
+    </div> */}
+        
         <nav className='links'>
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/About'>About</NavLink>
@@ -15,6 +35,8 @@ export const Navbar = () => {
             {/* <NavLink to='/Contact'>Contact</NavLink> */}
             
         </nav>
+
+        { view ? responsivelinks : ""}
     </>
   )
 }
